e9e92f9da4d917ac309f86bffceb2e00e149c534
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-04-23 15:33:32 +0100

    Fixed a bug in the table of compound interest responsivity

--- src/components/hero-section/TableRow.jsx
+++ src/components/hero-section/TableRow.jsx
@@ -2,7 +2,11 @@ export default function TableRow({ year, currentInterest, compoundInterest }) {
     return (
         <tr className="block p-2 [&:not(:last-child)]:border-b border-[#D6D6D6]">
             <td className="w-[50px]">{year}</td>
-            <td className="w-[150px]">{currentInterest}</td>
+            <td className="w-[150px]" title={currentInterest}>
+                {String(currentInterest).length <= 4
+                    ? currentInterest
+                    : String(currentInterest).slice(0, 5) + "..."}
+            </td>
             <td className="w-[200px]" title={compoundInterest}>
                 {String(compoundInterest).length <= 4
                     ? compoundInterest
