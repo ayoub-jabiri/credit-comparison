7836ab98ee4a6d369e57bec2fe236ebe6d5c20fa
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-04-24 11:29:04 +0100

    Fixed and updated the compound interest bonus part logic

--- src/components/hero-section/CompoundInterest.jsx
+++ src/components/hero-section/CompoundInterest.jsx
@@ -2,36 +2,43 @@ import TableRow from "./TableRow";
 
 export default function CompoundInterest({ card }) {
     function calculateCompound(years) {
-        return Math.pow(card.initial_amount * (1 + card.interest_rate), years);
+        return (
+            card.initial_amount * Math.pow(1 + card.interest_rate, years)
+        ).toFixed(2);
     }
 
     let yearsList = [];
 
-    let currentInterest = card.interest_rate;
+    let currentAmount = card.initial_amount;
 
     for (let i = 1; i <= card.duration; i++) {
-        currentInterest *= i;
+        let currentInterest = (currentAmount * card.interest_rate) / 100;
+        currentAmount += currentInterest;
 
         yearsList.push(
             <TableRow
                 key={i}
                 year={i}
-                currentInterest={currentInterest}
+                currentInterest={currentInterest.toFixed(2)}
+                total={currentAmount.toFixed(2)}
                 compoundInterest={calculateCompound(i)}
             />
         );
     }
 
     return (
-        <table className="block text-center border border-[#D6D6D6] overflow-auto">
-            <thead className="block w-full">
-                <tr className="block bg-[#F1F1F1] min-w-[400px] w-full p-2 border-b border-[#D6D6D6]">
+        <table className="block text-center overflow-auto">
+            <thead className="block w-fit">
+                <tr className="block bg-[#F1F1F1] min-w-[500px] w-full p-2 border border-[#D6D6D6]">
                     <th className="w-[50px]">Year</th>
-                    <th className="w-[150px]">Current Interest</th>
-                    <th className="w-[200px]">Total</th>
+                    <th className="w-[150px]">Interests</th>
+                    <th className="w-[150px]">Total</th>
+                    <th className="w-[150px]">Compound Interest</th>
                 </tr>
             </thead>
-            <tbody className="block min-w-[400px] w-full">{yearsList}</tbody>
+            <tbody className="block min-w-[500px] w-fit border-l border-r border-b border-[#D6D6D6]">
+                {yearsList}
+            </tbody>
         </table>
     );
 }
--- src/components/hero-section/TableRow.jsx
+++ src/components/hero-section/TableRow.jsx
@@ -1,16 +1,24 @@
-export default function TableRow({ year, currentInterest, compoundInterest }) {
+export default function TableRow({
+    year,
+    currentInterest,
+    total,
+    compoundInterest,
+}) {
+    function fixNumber(num) {
+        return String(num).length <= 7 ? num : String(num).slice(0, 7) + "...";
+    }
+
     return (
         <tr className="block p-2 [&:not(:last-child)]:border-b border-[#D6D6D6]">
             <td className="w-[50px]">{year}</td>
             <td className="w-[150px]" title={currentInterest}>
-                {String(currentInterest).length <= 4
-                    ? currentInterest
-                    : String(currentInterest).slice(0, 5) + "..."}
+                {fixNumber(currentInterest)}
             </td>
-            <td className="w-[200px]" title={compoundInterest}>
-                {String(compoundInterest).length <= 4
-                    ? compoundInterest
-                    : String(compoundInterest).slice(0, 5) + "..."}
+            <td className="w-[150px]" title={total}>
+                {fixNumber(total)}
+            </td>
+            <td className="w-[150px]" title={compoundInterest}>
+                {fixNumber(compoundInterest)}
             </td>
         </tr>
     );
